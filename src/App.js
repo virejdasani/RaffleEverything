9e3842259168198c3a6865c296e36fbdc3c4fbec
Virej Dasani <dasanivirej@gmail.com>
2020-12-12 09:45:23 +0530

    Added authentication

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Posts from './Post';
-import Button from '@material-ui/core/Button';
+import { Button } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
 import { db } from './firebase';
+import About from './About';
+import { Input } from '@material-ui/core';
+
+// Styling for modal
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
-
-
+// For the colors in things like buttons
 const theme = createMuiTheme({
-  // For the colors in things like buttons
   palette: {
     primary: {
       light: '#757ce8',
@@ -42,7 +68,17 @@ const theme = createMuiTheme({
     },
 });
 
+  // For modal
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+  // For posts
   const [posts, setPosts] = useState([]);
+  // For authentication
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState(''); 
+
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
@@ -54,27 +90,82 @@ const theme = createMuiTheme({
     })
   }, []);
 
+  const signUp = (event) => {
+
+  }
+
   return (
     <div className="app">
+
+      {/* SIGN-UP MODAL (POP-UP) */}
+      <Modal 
+      open={open}
+      onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="signUpForm">
+            <center>
+              <h3>Register</h3>
+              <div className="signUpInputFields">
+                {/* EMAIL */}
+                <Input
+                  placeholder="email"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)} />
+                  {/* PASSWORD */}
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+                  {/* CONFIRM PASSWORD */}
+                  <Input
+                    placeholder="Confirm Password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+
+                  <Button onClick={signUp}>Sign Up</Button>
+                </div>
+            </center>
+          </form>
+        </div>
+      </Modal>
+      {/* sign-up modal (pop-up) */}
 
       {/*---------- HEADER ----------*/}
       <div className="appHeader">
         {/* TODO  Site Logo, left aligned with style: object-fit: contain; so it doesn't stretch*/}
-        
-        {/* About button */}
-        <div className="aboutButton">
-          <MuiThemeProvider theme={theme}>
-            <span className="enterRaffleButton">
-              <Box pt={2}> {/* This will add 3px padding-top */}
-                <Button className="enterButton" variant="contained" size="small" disableElevation>
-                  ABOUT
-                </Button>
-              </Box>
-            </span>
-          </MuiThemeProvider>
-        </div> 
+  
+        {/* ABOUT BUTTON */}    
+        <table>
+          <th>
+            <div className="aboutButton">
+              <MuiThemeProvider theme={theme}>
+                <span className="enterRaffleButton">
+                  <Box pt={2}> {/* This will add 3px padding-top */}
+                    <Button onClick={() => {setOpen(true)}} className="enterButton" variant="contained" size="small" disableElevation>
+                      ABOUT
+                    </Button>
+                  </Box>
+                </span>
+              </MuiThemeProvider>
+            </div> 
+          </th>
+        </table>
+        {/* about button */}
 
         <h1 className="title">Raffle Everything!</h1>
+
+        {/* AUTHENTICATION */}
+        <div className="authOptions">
+          <div className="signUpButton">
+            <Button variant="outlined" color="secondary" size="medium" disableElevation onClick={() => {setOpen(true)}}> 
+              Sign Up
+            </Button>
+          </div>
+        </div>
+        {/* authentication */}
 
       </div>
       {/*---------- header ----------*/}
