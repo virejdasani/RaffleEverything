3b37e22f07dc927dc501d0249c9712355b6dc842
Virej Dasani <dasanivirej@gmail.com>
2020-12-19 15:18:03 +0530

    Made About modal look better

--- src/App.js
+++ src/App.js
@@ -234,24 +234,24 @@ const theme = createMuiTheme({
       open={openAbout}
       onClose={() => setOpenAbout(false)}>
         <div style={modalStyle} className={classes.paper}>
-          <h3 className="aboutModalTitle">
+          <h2 className="aboutModalTitle">
             About Raffle Everything
-          </h3>
-          <h4 className="aboutModalContent">
+          </h2>
+          <h3 className="aboutModalContent">
             • Each item on the website can be won by transferring the said amount via paytm
-          </h4>
-          <h4 className="aboutModalContent">  
+          </h3>
+          <h3 className="aboutModalContent">  
             • You can also buy additional raffle tickets to increase your chance of winning
-          </h4>
-          <h4 className="aboutModalContent">
+          </h3>
+          <h3 className="aboutModalContent">
             • Once the payment has been recieved, you will automatically be entered in the contest.
-          </h4>
-          <h4 className="aboutModalContent">
+          </h3>
+          <h3 className="aboutModalContent">
             • Win or lose, you are doing a good deed because all profits go towards efforts for stopping covid-19 at the end of each month.
-          </h4>
-          <h4 className="aboutModalContent">
+          </h3>
+          <h3 className="aboutModalContent">
             • If you win, you will receive a mail or message for your shipping info so we can deliver the product to you for FREE!
-          </h4>
+          </h3>
         </div>
       </Modal>
       {/* about modal */}
